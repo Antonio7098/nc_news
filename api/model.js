f16d145cd1310e15c62c18230f1597b30dfe55aa
Antonio <antoniorees74@gmail.com>
2025-04-30 15:15:53 +0100

    Completed 10 sort articles

--- api/model.js
+++ api/model.js
@@ -1,5 +1,6 @@
 const db = require("../db/connection")
 const format = require("pg-format")
+const { sort } = require("../db/data/test-data/articles")
 
 function selectTopics() {
     return db.query("SELECT * FROM topics").then((response) => {
@@ -19,15 +20,28 @@ function selectArticle(articleId) {
         })
 }
 
-function selectArticles() {
-    return db.query("SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id)::INT AS comment_count FROM articles LEFT JOIN  comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.created_at DESC")
+function selectArticles(sortBy, order) {
+    const validColumns = ['article_id', 'title', 'topic', 'author', 'created_at', 'votes', 'article_img_url']
+    const validOrders = ['ASC', 'DESC']
+
+    if (!validColumns.includes(sortBy)) {
+        return Promise.reject({status: 400, msg: "Bad request: Invalid column name"})
+    }
+    if (!validOrders.includes(order)) {
+        return Promise.reject({ status: 400, msg: "Bad request: Invalid order" });
+    }
+
+    return db.query(`SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id)::INT AS comment_count FROM articles LEFT JOIN  comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.${sortBy} ${order}`)
         .then((res) => {
             const articles = res.rows
             return articles
         })
+        .catch((err) => {
+            console.log(err)
+        })
 }
 
-function selectArticleComments(articleId) {
+function selectArticleComments(articleId, sortBy, order) {
     const selectComments = db.query("SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at ASC", [articleId])
     const checkArticle = selectArticle(articleId)
 
